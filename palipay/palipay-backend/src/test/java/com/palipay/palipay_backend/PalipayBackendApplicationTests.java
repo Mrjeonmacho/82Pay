d4f970e5b7da76022495aa9cc4342949abe62e6f
junyonglee0223 <ljyong3339@gmail.com>
2026-03-06 15:46:03 +0900

    chore: palipay-backend init + 전역 gitignore 추가

---
+++ palipay/palipay-backend/src/test/java/com/palipay/palipay_backend/PalipayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palipay.palipay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalipayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
