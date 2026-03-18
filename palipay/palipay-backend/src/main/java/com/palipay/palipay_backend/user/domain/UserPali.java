f5ea6fe110eee20757ac1a268ff523f66a517335
배현지 <qo0723@naver.com>
2026-03-18 13:25:42 +0900

    Fix: palipay 유저 부분 일부 수정

--- palipay/palipay-backend/src/main/java/com/palipay/palipay_backend/user/domain/UserPali.java
+++ palipay/palipay-backend/src/main/java/com/palipay/palipay_backend/user/domain/UserPali.java
@@ -5,8 +5,6 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -42,11 +40,9 @@ public class UserPali {
     @Column(name = "country_code", length = 10)
     private String countryCode;
 
-    @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
