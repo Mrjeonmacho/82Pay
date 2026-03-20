82dced24d4d50b68d253928ce0b071736eefb9c3
wjsrlwjd <wjsrlwjd@kookmin.ac.kr>
2026-03-20 14:28:52 +0900

    Feat: checkDto 수정

--- worldbank/worldbank_backend/src/main/java/com/worldbank/worldbank_backend/finance/domain/dto/Check/CheckRequestDto.java
+++ worldbank/worldbank_backend/src/main/java/com/worldbank/worldbank_backend/finance/domain/dto/Check/CheckRequestDto.java
@@ -3,12 +3,10 @@ package com.worldbank.worldbank_backend.finance.domain.dto.Check;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class CheckRequestDto {
     String targetAccountNumber;
     String targetAccountName;
