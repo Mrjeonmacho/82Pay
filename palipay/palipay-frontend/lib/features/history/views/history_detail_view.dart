93855c817dd972237c89ac61ecfa7dc9a15692c4
yeji <hyi7043@naver.com>
2026-03-28 20:23:01 +0900

    fix: 거래 내역 상세 화면 번역 수정

--- palipay/palipay-frontend/lib/features/history/views/history_detail_view.dart
+++ palipay/palipay-frontend/lib/features/history/views/history_detail_view.dart
@@ -202,7 +202,7 @@ class HistoryDetailView extends StatelessWidget {
 
             // 5. 하단 홈 버튼
             PaliButton(
-              text: 'common.btn_back'.tr(),
+              text: 'result.btn_back_to_home'.tr(),
               backgroundColor: AppColors.mainBlue,
               onPressed: () => Navigator.pop(context),
             ),
